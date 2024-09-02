e5b6b9bdcf07e618d1988fa58d47eb053aa2e939
Ceowyllian <downwell@online.ua>
2024-09-02 17:27:49 +0300

    feat(api): add common api tools

---
+++ src/api/common/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+
+class ModelSerializer(serializers.ModelSerializer):
+    def save(self, **kwargs):
+        raise NotImplementedError  # pragma: no cover
+
+    def create(self, validated_data):
+        raise NotImplementedError  # pragma: no cover
+
+    def update(self, instance, validated_data):
+        raise NotImplementedError  # pragma: no cover
