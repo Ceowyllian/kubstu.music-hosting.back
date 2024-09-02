cc8479d8b12051fe70f1d90e7dbf6473b3e41f3a
Ceowyllian <downwell@online.ua>
2024-09-02 14:56:53 +0300

    feat(config): add a settings file for rest_framework

---
+++ src/config/components/rest.py
@@ -0,0 +1,11 @@
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
+    # "EXCEPTION_HANDLER": "core.exception_handlers.exception_handler",
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
