6fce200459ba449185e0d504b81a8832b7c020eb
Ceowyllian <downwell@online.ua>
2024-09-06 10:30:14 +0300

    feat(social): add a db model for a person

---
+++ src/db/social/models/person.py
@@ -0,0 +1,48 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from db.common import BaseModel
+
+__all__ = [
+    "Person",
+]
+
+User = get_user_model()
+
+
+class Person(BaseModel):
+
+    user = models.OneToOneField(
+        to=User,
+        editable=False,
+        on_delete=models.RESTRICT,
+        verbose_name=_("User"),
+    )
+    avatar = models.ImageField(
+        upload_to="avatars",
+        null=True,
+        blank=True,
+        editable=True,
+        verbose_name=_("Avatar"),
+    )
+    summary = models.CharField(
+        null=True,
+        blank=True,
+        verbose_name=_("Summary"),
+    )
+    public_email = models.EmailField(
+        null=True,
+        blank=True,
+        editable=True,
+        verbose_name=_("Public email"),
+    )
+    saved_playlists = models.ManyToManyField(
+        to="music.Playlist",
+        related_name="followers",
+        related_query_name="follower",
+    )
+
+    class Meta:
+        verbose_name = _("Person")
+        verbose_name_plural = _("People")
