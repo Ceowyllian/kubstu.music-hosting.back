b3f22caf83b87863a335339a02a1ee623b0343d9
Ceowyllian <downwell@online.ua>
2024-09-17 13:46:36 +0300

    feat(api): add common schema tags

--- src/api/common/permissions.py
+++ src/api/common/permissions.py
@@ -2,6 +2,10 @@ from rest_framework.permissions import BasePermission
 
 from db.person.models import WithOwnerMixin
 
+__all__ = [
+    "IsOwner",
+]
+
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj: WithOwnerMixin):
---
+++ src/api/common/schema_tags.py
@@ -0,0 +1,10 @@
+from django.utils.translation import gettext_lazy as _
+
+__all__ = [
+    "SCHEMA_TAG_MUSIC",
+    "SCHEMA_TAG_SOCIAL",
+]
+
+
+SCHEMA_TAG_MUSIC = _("Music")
+SCHEMA_TAG_SOCIAL = _("Social")
