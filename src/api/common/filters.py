c69f6fad95ff7b9661395a011327fa9de39fdb6c
Ceowyllian <downwell@online.ua>
2024-09-17 14:02:24 +0300

    feat(api): add common filters

---
+++ src/api/common/filters.py
@@ -0,0 +1,8 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
+
+__all__ = [
+    "SearchFilter",
+    "OrderingFilter",
+    "DjangoFilterBackend",
+]
