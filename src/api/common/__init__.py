a2b9062d0d877dc50fca92ac37703fcc2b50be33
Ceowyllian <downwell@online.ua>
2024-09-17 14:05:12 +0300

    feat(api): add common responses

--- src/api/common/__init__.py
+++ src/api/common/__init__.py
@@ -2,6 +2,7 @@ from .fields import *  # noqa
 from .filters import *  # noqa
 from .open_api import *  # noqa
 from .permissions import *  # noqa
+from .response import *  # noqa
 from .schema_tags import *  # noqa
 from .serializers import *  # noqa
 from .views import *  # noqa
---
+++ src/api/common/response.py
@@ -0,0 +1,5 @@
+from rest_framework.response import Response
+
+__all__ = [
+    "Response",
+]
