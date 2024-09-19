cd1d02159750701bfcc24e44eeb9b4c55e480c9f
Ceowyllian <downwell@online.ua>
2024-09-19 14:15:19 +0300

    feat(api): add filter fields to api.common

--- src/api/common/filters/__init__.py
+++ src/api/common/filters/__init__.py
@@ -1,8 +1,12 @@
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 from rest_framework.filters import OrderingFilter, SearchFilter
 
+from . import filter_fields
+
 __all__ = [
+    "FilterSet",
     "SearchFilter",
     "OrderingFilter",
     "DjangoFilterBackend",
+    "filter_fields",
 ]
