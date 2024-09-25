9da5c3886137f885660d570bd483c3646247e0c6
Ceowyllian <downwell@online.ua>
2024-09-25 11:24:55 +0300

    feat(api): add common parsers

--- src/api/common/__init__.py
+++ src/api/common/__init__.py
@@ -2,6 +2,7 @@ from . import fields
 from .action import *  # noqa
 from .filters import *  # noqa
 from .open_api import *  # noqa
+from .parsers import *  # noqa
 from .permissions import *  # noqa
 from .response import *  # noqa
 from .schema_tags import *  # noqa
---
+++ src/api/common/parsers.py
@@ -0,0 +1,13 @@
+from rest_framework.parsers import (
+    FileUploadParser,
+    FormParser,
+    JSONParser,
+    MultiPartParser,
+)
+
+__all__ = [
+    "FileUploadParser",
+    "FormParser",
+    "JSONParser",
+    "MultiPartParser",
+]
