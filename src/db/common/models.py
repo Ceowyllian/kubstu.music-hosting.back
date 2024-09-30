4d0558f04f58efe1739a5e8fcf551c1928b07c1d
Ceowyllian <downwell@online.ua>
2024-09-30 11:17:39 +0300

    feat(db)!: make all models soft-deletable

--- src/db/common/models.py
+++ src/db/common/models.py
@@ -2,7 +2,7 @@ import uuid
 
 from django.contrib.auth import get_user_model
 from model_utils.fields import UUIDField
-from model_utils.models import TimeStampedModel
+from model_utils.models import SoftDeletableModel, TimeStampedModel
 
 __all__ = [
     "BaseModel",
@@ -11,7 +11,10 @@ __all__ = [
 User = get_user_model()
 
 
-class BaseModel(TimeStampedModel):
+class BaseModel(
+    TimeStampedModel,
+    SoftDeletableModel,
+):
     id = UUIDField(default=uuid.uuid4)
 
     class Meta:
