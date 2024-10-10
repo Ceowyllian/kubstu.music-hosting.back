572d6c1380ea059b3ce622df6141d834137cba2e
Ceowyllian <downwell@online.ua>
2024-10-10 12:41:23 +0300

    feat(social): add services to create and destroy likes

---
+++ src/services/social/like.py
@@ -0,0 +1,27 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework.exceptions import ValidationError
+
+from db.social.models import Like
+
+__all__ = [
+    "like_create",
+    "like_destroy",
+]
+
+
+def like_create(*, liked_by, target_id, target_type):
+    like = Like(
+        target_id=target_id,
+        target_type=target_type,
+        owner=liked_by,
+    )
+    like.full_clean()
+    like.save()
+
+
+def like_destroy(*, liked_by, target_id):
+    try:
+        like = Like.available_objects.get(target_id=target_id, owner=liked_by)
+    except Like.DoesNotExist as e:
+        raise ValidationError(_("You have not liked this post yet.")) from e
+    like.delete(soft=False)
