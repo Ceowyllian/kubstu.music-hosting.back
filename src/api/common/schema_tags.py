39221a8aedc1ea6d67debe4ebaaeaa92cdc852c3
Ceowyllian <downwell@online.ua>
2024-11-06 12:07:10 +0300

    feat(api): add a schema tag for comment views

--- src/api/common/schema_tags.py
+++ src/api/common/schema_tags.py
@@ -4,9 +4,11 @@ __all__ = [
     "SCHEMA_TAG_MUSIC",
     "SCHEMA_TAG_SOCIAL",
     "SCHEMA_TAG_LIKES",
+    "SCHEMA_TAG_COMMENTS",
 ]
 
 
 SCHEMA_TAG_MUSIC = _("Music")
 SCHEMA_TAG_SOCIAL = _("Social")
 SCHEMA_TAG_LIKES = _("Likes")
+SCHEMA_TAG_COMMENTS = _("Commentaries")
