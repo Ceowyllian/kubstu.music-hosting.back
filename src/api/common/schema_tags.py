7ea98a5d3fc5132b9fc356d2819abfacbb52de17
Ceowyllian <downwell@online.ua>
2024-11-12 13:52:36 +0300

    feat(api): add SCHEMA_TAG_MY to api.comon

--- src/api/common/schema_tags.py
+++ src/api/common/schema_tags.py
@@ -5,6 +5,7 @@ __all__ = [
     "SCHEMA_TAG_SOCIAL",
     "SCHEMA_TAG_LIKES",
     "SCHEMA_TAG_COMMENTS",
+    "SCHEMA_TAG_MY",
 ]
 
 
@@ -12,3 +13,4 @@ SCHEMA_TAG_MUSIC = _("Music")
 SCHEMA_TAG_SOCIAL = _("Social")
 SCHEMA_TAG_LIKES = _("Likes")
 SCHEMA_TAG_COMMENTS = _("Commentaries")
+SCHEMA_TAG_MY = _("My")
