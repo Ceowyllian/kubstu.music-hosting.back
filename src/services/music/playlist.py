dc568de510fc531976b4413c6522100416a3c48d
Ceowyllian <downwell@online.ua>
2024-11-13 11:44:34 +0300

    refactor(api): remove obsolete playlist service functions

--- src/services/music/playlist.py
+++ src/services/music/playlist.py
@@ -1,14 +1,12 @@
 from django.db import transaction
 
-from db.music.models import Playlist, PlaylistTrack
+from db.music.models import Playlist
 from services.common import model_update
 
 __all__ = [
     "playlist_create",
     "playlist_update",
     "playlist_destroy",
-    "playlist_add_track",
-    "playlist_remove_track",
 ]
 
 
@@ -35,14 +33,3 @@ def playlist_update(*, playlist: Playlist, name: str):
 def playlist_destroy(playlist: Playlist):
     playlist.delete()
     playlist.tracks.all().delete()
-
-
-def playlist_add_track(*, playlist_id, track_id):
-    track_in_playlist = PlaylistTrack(playlist_id=playlist_id, track_id=track_id)
-    track_in_playlist.full_clean()
-    track_in_playlist.save()
-    return track_in_playlist.track
-
-
-def playlist_remove_track(*, playlist_id, track_id):
-    PlaylistTrack.objects.get(playlist_id=playlist_id, track_id=track_id).delete()
