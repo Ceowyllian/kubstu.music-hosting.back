0227db9bcc9b20833ae955938cc5d08c54e59dee
Ceowyllian <downwell@online.ua>
2024-11-21 11:47:51 +0300

    feat(api): add SCHEMA_TAG_SEARCH to api.common

--- src/api/common/schema_tags.py
+++ src/api/common/schema_tags.py
@@ -6,6 +6,7 @@ __all__ = [
     "SCHEMA_TAG_LIKES",
     "SCHEMA_TAG_COMMENTS",
     "SCHEMA_TAG_MY",
+    "SCHEMA_TAG_SEARCH",
 ]
 
 
@@ -14,3 +15,4 @@ SCHEMA_TAG_SOCIAL = _("Social")
 SCHEMA_TAG_LIKES = _("Likes")
 SCHEMA_TAG_COMMENTS = _("Commentaries")
 SCHEMA_TAG_MY = _("My")
+SCHEMA_TAG_SEARCH = _("Search")
