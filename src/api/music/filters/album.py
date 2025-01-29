46d879a912c68d221ca559252ad2263def18b649
Ceowyllian <downwell@online.ua>
2025-01-29 11:22:21 +0300

    feat(music): add "exclude_track_id" search param

--- src/api/music/filters/album.py
+++ src/api/music/filters/album.py
@@ -1,4 +1,4 @@
-from api.common import FilterSet, base_model_fields, owner_fields
+from api.common import FilterSet, base_model_fields, filter_fields, owner_fields
 from db.music.models import Album
 
 __all__ = [
@@ -7,6 +7,13 @@ __all__ = [
 
 
 class AlbumFilterSet(FilterSet):
+    exclude_track_id = filter_fields.UUIDFilter(
+        method="filter_exclude_track_id",
+    )
+
+    def filter_exclude_track_id(self, queryset, _, value):
+        return queryset.exclude(albumtrack__track__id=value)
+
     class Meta:
         model = Album
         fields = {
--- src/api/music/filters/playlist.py
+++ src/api/music/filters/playlist.py
@@ -1,4 +1,4 @@
-from api.common import FilterSet, base_model_fields, owner_fields
+from api.common import FilterSet, base_model_fields, filter_fields, owner_fields
 from db.music.models import Playlist
 
 __all__ = [
@@ -7,6 +7,13 @@ __all__ = [
 
 
 class PlaylistFilterSet(FilterSet):
+    exclude_track_id = filter_fields.UUIDFilter(
+        method="filter_exclude_track_id",
+    )
+
+    def filter_exclude_track_id(self, queryset, _, value):
+        return queryset.exclude(playlisttrack__track__id=value)
+
     class Meta:
         model = Playlist
         fields = {
